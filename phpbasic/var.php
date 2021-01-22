492958d33c75123982a260d02192e6a679dfa90c
bhargavi5299 <63501250+bhargavi5299@users.noreply.github.com>
2021-01-22 19:03:54 +0530

    22-01-2021

---
+++ phpbasic/var.php
@@ -0,0 +1,32 @@
+<?php  
+$str="hello string";  
+$x=200;  
+$y=44.6;  
+echo "string is: $str <br/>";  
+echo "integer is: $x <br/>";  
+echo "float is: $y <br/>";  
+
+
+
+//*********case senstivive*************
+ 
+/*$color="red";  
+echo "My car is " . $color . "<br>";  
+echo "My house is " . $COLOR . "<br>";  
+echo "My boat is " . $coLOR . "<br>";  */ 
+
+
+
+$a="hello";//letter (valid)  
+$_b="hello";//underscore (valid)  
+  
+echo "$a <br/> $_b";
+
+
+
+
+/*$4c="hello";//number (invalid)  
+$*d="hello";//special symbol (invalid)  
+  
+echo "$4c <br/> $*d";  */
+?>  
